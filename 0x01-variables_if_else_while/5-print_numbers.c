2cb1ad17ee56ec5be1ec728614f9f3216b3c041d
koketsol <koketsolesiea@gmail.com>
2023-08-11 20:13:11 -0700

    print numbers of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers of base 10
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf(num);
+
+	printf("\n");
+
+	return (0);
+}
