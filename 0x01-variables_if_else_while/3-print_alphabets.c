88b26257a9d9322bdcf8086666134c965b11b4ea
koketsol <koketsolesiea@gmail.com>
2023-08-11 19:55:13 -0700

    prints lowe and uppercase alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints lower and uppercase alphabets
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char y;
+	char z;
+
+	for (y = 'a'; y < 'z'; y++)
+		putchar(y);
+
+	for (z = 'A'; z < 'Z'; z++)
+		putchar(z);
+
+	putchar('\n');
+	return (0);
+}
