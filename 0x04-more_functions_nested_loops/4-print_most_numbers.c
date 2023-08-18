e486a27a2698cac1a6ecd6419c36b7ef335efa4f
koketsol <koketsolesiea@gmail.com>
2023-08-18 13:59:56 -0700

    fixed indentation

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,7 +10,11 @@ void print_most_numbers(void)
 	int a;
 
 	for (a = '0'; a <= '9'; a++)
-	if (a != '2' && a != '4')
-		_putchar(a);
-		_putchar('\n');
+	{
+		if (a != '2' && a != '4')
+		{
+			_putchar(a);
+			_putchar('\n');
+		}
+	}
 }
